23b3ebd4b2248073e2a960b548fedabaefcf159a
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-09-27 20:35:33 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommunityLinkController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [CommunityLinkController::class, 'index']);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
