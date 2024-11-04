d445e94da1c00d18a51aa3cdea590303e5a1fc69
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-11-04 17:21:57 +0000

    Add files via upload

--- resources/views/components/community-links.blade.php
+++ resources/views/components/community-links.blade.php
@@ -1,2 +1,28 @@
 @props(['links'])
-<h1>Añade un Link</h1>
+<div class="flex mb-4 max-w-7xl">
+    <div class="w-1/2 p-2 bg-gray-800 text-white rounded-md ">
+        @if ($links->isEmpty())
+        <p class="bg-gray-800 text-white rounded-md">{{ __("No approved contributions yet!") }}</p>
+        @else
+        @foreach ($links as $link)
+        <li>{{$link->title}}</li>
+        <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
+
+        @endforeach
+        @endif
+
+        {{$links->links()}}
+
+    </div>
+    <div class="w-1/2 p-2 text-white rounded-md">
+        @if (session('linkSend'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            {{ session('linkSend') }}
+        </div>
+        @else
+        <div class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative" role="alert">
+            Pendiente de aprobación
+        </div>
+        @endif
+    </div>
+</div>
